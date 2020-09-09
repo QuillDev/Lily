17d9ca620f884819d1d1a4c08072760b007a553d
Robert Brunson <siinspam@gmail.com>
2020-09-09 18:34:32 -0400

    Remove ignored files

---
+++ cmake-build-debug/CMakeFiles/Lily__.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lily__.dir/main.cpp.o"
+  "Lily__"
+  "Lily__.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lily__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
