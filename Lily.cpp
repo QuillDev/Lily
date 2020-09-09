37fc3941a67d09b0d64706ae972148e55ecfdcb3
Robert Brunson <siinspam@gmail.com>
2020-09-09 18:04:41 -0400

    added nlhomann_json

--- Lily.cpp
+++ Lily.cpp
@@ -1,6 +1,31 @@
-#include <iostream>
+//Include websocket stuff from sleepy-discord
+#include "sleepy_discord/websocketpp_websocket.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+//get strings
+#include <string>
+
+class Bot : SleepyDiscord::DiscordClient {
+public:
+    using SleepyDiscord::DiscordClient::DiscordClient;
+
+    void onMessage(SleepyDiscord::Message message){
+
+        //checks on message to see if it's worth looking at
+        if(message.author.bot) return;
+        if(!message.startsWith(">")) return; //TODO Load prefix from file
+
+        //get the id of the channel, type is SleepyDiscord::Snowflake<Channel>
+        auto channelID = message.channelID;
+
+        //save the message content to a string
+        std::string content = message.content;
+
+        //send the message to the channel
+        sendMessage(channelID, content);
+    }
+};
+
+int main(){
+
 }
