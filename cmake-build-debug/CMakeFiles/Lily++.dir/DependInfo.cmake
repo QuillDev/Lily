e13f984555eb5b33fa76e6873637341d40e05bb9
Robert Brunson <siinspam@gmail.com>
2020-09-09 17:29:50 -0400

    base project

---
+++ cmake-build-debug/CMakeFiles/Lily++.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/quill/CLionProjects/Lily++/Lily.cpp" "/home/quill/CLionProjects/Lily++/cmake-build-debug/CMakeFiles/Lily++.dir/Lily.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CURL_STATICLIB"
+  "EXISTENT_ASIO"
+  "EXISTENT_CPR"
+  "EXISTENT_WEBSOCKETPP"
+  "NONEXISTENT_OPUS"
+  "NONEXISTENT_SODIUM"
+  "NONEXISTENT_UWEBSOCKETS"
+  "SLEEPY_DISCORD_CMAKE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external/sleepy-discord/include"
+  "../external/sleepy-discord/deps/include"
+  "../external/sleepy-discord/deps/cpr/include"
+  "../external/sleepy-discord/deps/asio/asio/include"
+  "../external/sleepy-discord/deps/websocketpp"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/quill/CLionProjects/Lily++/cmake-build-debug/external/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/DependInfo.cmake"
+  "/home/quill/CLionProjects/Lily++/cmake-build-debug/external/sleepy-discord/deps/cpr/cpr/CMakeFiles/cpr.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Lily++.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lily++.dir/Lily.cpp.o"
+  "Lily++"
+  "Lily++.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lily++.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
